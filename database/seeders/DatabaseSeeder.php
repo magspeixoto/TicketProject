56940bfdd3b10e3cdaccfe34dc901ab27a757356
Margarida <margarida@Margaridas-Mac-mini.local>
2024-08-08 13:20:04 +0100

    design improvement part2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\ChatRoom;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -35,6 +37,47 @@ class DatabaseSeeder extends Seeder
             'email' => 'user@user.com',
             'is_admin' => false
         ]);
+        // Create roles
+        $administrator = Role::create(['name' => 'Administrator']);
+        $manager = Role::create(['name' => 'Manager']);
+        $agent = Role::create(['name' => 'Agent']);
+        $customer = Role::create(['name' => 'Customer']);
 
+        // Create permissions
+        $permissions = [
+            // Administrator permissions
+            'create users', 'read users', 'update users', 'delete users',
+            'create tickets', 'read tickets', 'update tickets', 'delete tickets',
+            'manage ticket statuses', 'manage ticket priorities', 'generate reports', 'modify configurations',
+
+            // Manager permissions
+            'create tickets', 'read tickets', 'update tickets', 'close tickets',
+            'assign tickets', 'view reports', 'manage ticket statuses', 'manage ticket priorities',
+
+            // Agent permissions
+            'create tickets', 'read tickets', 'update tickets', 'close tickets',
+            'add comments',
+
+            // Customer permissions
+            'create tickets', 'read own tickets',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Assign permissions to roles
+        $administrator->givePermissionTo(Permission::all());
+        $manager->givePermissionTo([
+            'create tickets', 'read tickets', 'update tickets', 'close tickets',
+            'assign tickets', 'view reports', 'manage ticket statuses', 'manage ticket priorities',
+        ]);
+        $agent->givePermissionTo([
+            'create tickets', 'read tickets', 'update tickets', 'close tickets',
+            'add comments',
+        ]);
+        $customer->givePermissionTo([
+            'create tickets', 'read own tickets', 'add comments',
+        ]);
     }
 }
